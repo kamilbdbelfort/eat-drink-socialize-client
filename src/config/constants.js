0a4a389b69a001eced212055605cc8d5a1510685
kamilbdbelfort <k.beelidebelfort@hotmail.com>
2021-10-19 17:30:23 +0200

    api url change

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,4 @@
-export const apiUrl = process.env.API_URL || "http://localhost:4000";
+export const apiUrl =
+  process.env.API_URL || "https://eat-drink-socialize-backend.herokuapp.com";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
 export const DEFAULT_PAGINATION_LIMIT = 10;
