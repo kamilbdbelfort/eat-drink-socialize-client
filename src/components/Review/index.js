f8550ba68e2e5995a073c6fe3c68890099448e5c
kamilbdbelfort <k.beelidebelfort@hotmail.com>
2021-10-29 11:57:38 +0200

    review comp name

--- src/components/Review/index.js
+++ src/components/Review/index.js
@@ -16,7 +16,7 @@ export default function Review(props) {
       <div style={{ width: "10%" }}></div>
       <Jumbotron className="Review-component">
         <h2>
-          {name} {showStars(rating)}
+          {name ? name : null} {showStars(rating)}
         </h2>
         <br />
         <h3>{title}</h3>
