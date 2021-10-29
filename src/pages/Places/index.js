8cd4cc63fb37a68837c4bf600e5ad5e32b087620
kamilbdbelfort <k.beelidebelfort@hotmail.com>
2021-10-29 14:40:42 +0200

    undo commits

--- src/pages/Places/index.js
+++ src/pages/Places/index.js
@@ -65,8 +65,6 @@ export default function Places() {
                   place={place.place}
                   name={place.name}
                   image={place.image}
-                  reviews={place.reviews}
-                  users={place.users}
                   showLink={true}
                 />
               </div>
